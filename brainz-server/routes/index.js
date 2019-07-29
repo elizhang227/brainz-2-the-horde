4e5421cd74763ecb495f5bf0b09f176b4661446c
elizhang227 <zhang.eli@hotmail.com>
2019-07-29 10:06:56 -0400

    setstate initially before socket so we dont have an empty scores page before setinterval runs

--- brainz-server/routes/index.js
+++ brainz-server/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const ScoresModel = require('../models/scores');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('test');
 });
+
+router.get('/scores', async (req, res, next) => {
+  const allScores = await ScoresModel.getHighScores();
+  res.json(allScores).status(200);
+})
 
 module.exports = router;
