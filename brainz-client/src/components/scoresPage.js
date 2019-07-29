b1c57bfbf21499f9266c3ebcc5aa3933d7b9e10f
elizhang227 <zhang.eli@hotmail.com>
2019-07-29 11:59:55 -0400

    added recent scores section so they can see the most recent scores of ppl that have played

--- brainz-client/src/components/scoresPage.js
+++ brainz-client/src/components/scoresPage.js
@@ -4,29 +4,54 @@ import io from 'socket.io-client';
 class Scores extends Component {
     state = {
         endpoint: '10.150.41.155:3000',
-        response: false,
-        initial: false
+        highscores: false,
+        recentscores: false,
+        onLoad: false
     }
 
     componentDidMount = async() => {
         // Load the scores initially before the setInterval is called in socket
-        const initialScores = await this.loadInitialScores();
-        this.setState({ response: initialScores });
+        const initialScores = await this.loadInitialHighScores();
+        const recentScores = await this.loadInitialRecentScores();
+        this.setState({ 
+            highscores: initialScores,
+            recentscores: recentScores
+        });
 
         const { endpoint } = this.state;
         const socket = io(endpoint);
-        socket.on('test', data => {
-            if (this.state.initial === false) {
-                this.setState({ response: data })
+
+        socket.on('highScores', data => {
+            if (this.state.onLoad === false) {
+                this.setState({ highscores: data })
                 this.componentWillUnmount = () => {
                     console.log('this is test to see if it works')
                     socket.disconnect();
                 }
             }
             console.log('this is data', data)
-            if (data.length > this.state.response.length) {
+            if (data.length > this.state.highscores.length) {
                 this.setState({
-                    response: data,
+                    highscores: data,
+                })
+                console.log('changed')
+            } else {
+                console.log('still the same')
+            }
+        })
+
+        socket.on('recentScores', data => {
+            if (this.state.onLoad === false) {
+                this.setState({ recentscores: data })
+                this.componentWillUnmount = () => {
+                    console.log('this is test to see if it works')
+                    socket.disconnect();
+                }
+            }
+            console.log('this is data', data)
+            if (data.length > this.state.recentscores.length) {
+                this.setState({
+                    recentscores: data,
                 })
                 console.log('changed')
             } else {
@@ -35,22 +60,42 @@ class Scores extends Component {
         })
     }
 
-    loadInitialScores = async () => {
-        const url = `http://10.150.41.155:3000/scores`;
+    loadInitialHighScores = async () => {
+        const url = `http://10.150.41.155:3000/highscores`;
+        const response = await fetch(url);
+        const data = response.json();
+        return data;
+    }
+
+    loadInitialRecentScores = async () => {
+        const url = `http://10.150.41.155:3000/recentscores`;
         const response = await fetch(url);
         const data = response.json();
         return data;
     }
 
     render() {
-        const { response } = this.state;
+        const { highscores, recentscores } = this.state;
         //console.log('this is response', response)
         return (
         <div>
-            <h2>f00k dis</h2>
-            {(response !== false) ? 
+            <h2>Top 10 Scores</h2>
+            {(highscores !== false) ? 
             <ul>
-            {response.map((data, index) => {
+            {highscores.map((data, index) => {
+                return (
+                <li key={`data${index}`}>
+                    USERID: {data.user_id} Wave: {data.wave} Kills: {data.kills}
+                </li>
+                )
+            })}
+            </ul>
+            : ''
+            }
+            <h2>Recent Games</h2>
+            {(recentscores !== false) ? 
+            <ul>
+            {recentscores.map((data, index) => {
                 return (
                 <li key={`data${index}`}>
                     USERID: {data.user_id} Wave: {data.wave} Kills: {data.kills}
