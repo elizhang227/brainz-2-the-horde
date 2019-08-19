3e7dbd729fb99afc7141aaf5dca03edf1433e5a6
elizhang227 <zhang.eli@hotmail.com>
2019-08-19 09:38:57 -0400

    un-ignoring build folder

---
+++ brainz-client/build/precache-manifest.372c5a8ae6bb742cb3b51bd771df09d9.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10d9dcef4efd3c34cb1ea26534071660",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f467ff2b1428d1884885",
+    "url": "/static/css/main.a7c7d56a.chunk.css"
+  },
+  {
+    "revision": "ca61a7d898ebf4e8bd24",
+    "url": "/static/js/2.0aa34dd7.chunk.js"
+  },
+  {
+    "revision": "f0ed85e35b949ca8aedb",
+    "url": "/static/js/3.4ab4bf98.chunk.js"
+  },
+  {
+    "revision": "e3fa3a47087f85711665",
+    "url": "/static/js/4.b2579587.chunk.js"
+  },
+  {
+    "revision": "4f1fc2541b612d6f3acc",
+    "url": "/static/js/5.8a45a8e6.chunk.js"
+  },
+  {
+    "revision": "87fd12de9728b6f2b97c",
+    "url": "/static/js/6.e649376a.chunk.js"
+  },
+  {
+    "revision": "f89fb0be9ea1f87fda1a",
+    "url": "/static/js/7.746808fb.chunk.js"
+  },
+  {
+    "revision": "b0d0bf59a5b8167761e1",
+    "url": "/static/js/8.f16a3144.chunk.js"
+  },
+  {
+    "revision": "f467ff2b1428d1884885",
+    "url": "/static/js/main.b5451a0a.chunk.js"
+  },
+  {
+    "revision": "7935d9574b3ca07ecd10",
+    "url": "/static/js/runtime~main.632d9d56.js"
+  },
+  {
+    "revision": "b10849c899982654fae3f01d02269043",
+    "url": "/static/media/Emulogic-zrEw.b10849c8.ttf"
+  },
+  {
+    "revision": "e35b0e8575b631cd27f3fa19b8fd1755",
+    "url": "/static/media/aim1.e35b0e85.mp4"
+  },
+  {
+    "revision": "ee6fdbed8057a8e028bd44b8a1b790a3",
+    "url": "/static/media/aim2.ee6fdbed.mp4"
+  },
+  {
+    "revision": "59feeb2ba810e467a6d69b407666f855",
+    "url": "/static/media/arcadev2.59feeb2b.png"
+  },
+  {
+    "revision": "ff0ccd1591dd1b63966214e52a0cba53",
+    "url": "/static/media/background1.ff0ccd15.png"
+  },
+  {
+    "revision": "1b997132edcfc4051c5879d971fe164f",
+    "url": "/static/media/backgroundv2.1b997132.jpg"
+  },
+  {
+    "revision": "e05763dfc6afd0858f70ae3fcdcc0b75",
+    "url": "/static/media/brainz-logo-1.e05763df.png"
+  },
+  {
+    "revision": "f8a365b325e9f8abc04f44a783476efd",
+    "url": "/static/media/computer-mouse.f8a365b3.gif"
+  },
+  {
+    "revision": "54630d45480f606b81344ad58596794f",
+    "url": "/static/media/gameover-once.54630d45.gif"
+  },
+  {
+    "revision": "0ab4e64e08a8120cdf2a1d28afb2ac0e",
+    "url": "/static/media/homepage.0ab4e64e.gif"
+  },
+  {
+    "revision": "3f8c3841c0203a21d1279f3a59d2b6ab",
+    "url": "/static/media/mouse-click.3f8c3841.gif"
+  },
+  {
+    "revision": "a4facf1a098be0c1abce04c9ec57857c",
+    "url": "/static/media/move1.a4facf1a.mp4"
+  },
+  {
+    "revision": "0486c6f2959e2c9567c366dde47b8f9b",
+    "url": "/static/media/move2.0486c6f2.mp4"
+  },
+  {
+    "revision": "eab43bdf6b283cae433cd381ed8688e4",
+    "url": "/static/media/shoot1.eab43bdf.mp4"
+  },
+  {
+    "revision": "6626abd8ad0065ef7fd6b21c920bda8b",
+    "url": "/static/media/shoot2.6626abd8.mp4"
+  },
+  {
+    "revision": "914287974e5d46ce9dce3e4fa77f8223",
+    "url": "/static/media/start.91428797.gif"
+  },
+  {
+    "revision": "cc3372003ffb05149170c5cc89f21e1f",
+    "url": "/static/media/wasd-fixed.cc337200.gif"
+  }
+]);
